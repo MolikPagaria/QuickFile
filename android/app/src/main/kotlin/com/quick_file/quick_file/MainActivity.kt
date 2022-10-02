74b9144683082d64ae9a9e9260289d29342901c4
FlutLab.io <>
2022-10-02 04:12:44 +0000

    UPDATE

--- android/app/src/main/kotlin/com/quick_file/quick_file/MainActivity.kt
+++ android/app/src/main/kotlin/com/quick_file/quick_file/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.quickfilemain
+package com.quick_file.quick_file
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,148 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:quick_file/controllers/auth_controller.dart';
+import 'package:quick_file/screens/home/home.dart';
+import 'package:quick_file/utils/utils.dart';
+import 'controllers/app_controller.dart';
+import 'screens/auth/sign_in.dart';
+import 'screens/home/index.dart';
+import 'splash.dart';
 
-void main() => runApp(MyApp());
+RxString authToken = "".obs;
+int? firstLunch;
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  importance: Importance.high,
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await GetStorage.init();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await initialization.then((value) {
+    Get.put(AuthController());
+    Get.put(AppController());
+  });
+
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  // final Future<FirebaseApp> _initFirebaseSdk = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return GetMaterialApp(
+      supportedLocales: const [
+        Locale("af"),
+        Locale("am"),
+        Locale("ar"),
+        Locale("az"),
+        Locale("be"),
+        Locale("bg"),
+        Locale("bn"),
+        Locale("bs"),
+        Locale("ca"),
+        Locale("cs"),
+        Locale("da"),
+        Locale("de"),
+        Locale("el"),
+        Locale("en"),
+        Locale("es"),
+        Locale("et"),
+        Locale("fa"),
+        Locale("fi"),
+        Locale("fr"),
+        Locale("gl"),
+        Locale("ha"),
+        Locale("he"),
+        Locale("hi"),
+        Locale("hr"),
+        Locale("hu"),
+        Locale("hy"),
+        Locale("id"),
+        Locale("is"),
+        Locale("it"),
+        Locale("ja"),
+        Locale("ka"),
+        Locale("kk"),
+        Locale("km"),
+        Locale("ko"),
+        Locale("ku"),
+        Locale("ky"),
+        Locale("lt"),
+        Locale("lv"),
+        Locale("mk"),
+        Locale("ml"),
+        Locale("mn"),
+        Locale("ms"),
+        Locale("nb"),
+        Locale("nl"),
+        Locale("nn"),
+        Locale("no"),
+        Locale("pl"),
+        Locale("ps"),
+        Locale("pt"),
+        Locale("ro"),
+        Locale("ru"),
+        Locale("sd"),
+        Locale("sk"),
+        Locale("sl"),
+        Locale("so"),
+        Locale("sq"),
+        Locale("sr"),
+        Locale("sv"),
+        Locale("ta"),
+        Locale("tg"),
+        Locale("th"),
+        Locale("tk"),
+        Locale("tr"),
+        Locale("tt"),
+        Locale("uk"),
+        Locale("ug"),
+        Locale("ur"),
+        Locale("uz"),
+        Locale("vi"),
+        Locale("zh")
+      ],
+      localizationsDelegates: const [],
+      title: appTitle,
+      debugShowCheckedModeBanner: false,
+      defaultTransition: Transition.rightToLeft,
+      theme: ThemeData(
+        scaffoldBackgroundColor: white,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        appBarTheme: const AppBarTheme(
+          elevation: appBarElevation,
         ),
       ),
+      home: Home(),
     );
   }
 }
